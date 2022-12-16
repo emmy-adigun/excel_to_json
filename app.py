d84382442769005a8c45a4feddfed136c7f85b4f
Adigun Emmanuel <adigunemmanuel@OLuwafemis-MacBook-Pro-2.local>
2022-12-16 10:41:00 +0100

    new script updates1

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if uploaded_file is not None:
 
 
      # Can be used wherever a "file-like" object is accepted:
-     excel_data_df = pd.read_excel(uploaded_file)
+     excel_data_df = pd.read_excel(uploaded_file, engine='openpyxl')
      json_str = excel_data_df.to_json('Products21.json', orient='records')
      
      st.markdown("### **Check your folder after upload**")
