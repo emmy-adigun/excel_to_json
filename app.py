c5c6a79949cf5428be3fbfdd0f08fb48196efbe1
Adigun Emmanuel <adigunemmanuel@OLuwafemis-MacBook-Pro-2.local>
2022-12-16 09:36:03 +0100

    new script updates

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-import matplotlib.pyplot as plt
 # from wordcloud import WordCloud
 
 st.set_page_config(layout="wide")
