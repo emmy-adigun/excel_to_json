e07a9505f79e71679a4f6ccfd23fed74c8c7d95a
Adigun Emmanuel <adigunemmanuel@OLuwafemis-MacBook-Pro-2.local>
2022-12-20 10:39:01 +0100

    new script update

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if uploaded_file is not None:
 
      # Can be used wherever a "file-like" object is accepted:
      excel_data_df = pd.read_excel(uploaded_file, skiprows=list(range(2)))
-     json_str = excel_data_df.to_json(path_or_buf='skillz.json', orient='records')
+     json_str = excel_data_df.to_json(path_or_buf='/Downloads', orient='records')
      
      st.markdown("#### **Check your download folder for the JSON file**")
      # st.write(json_str)
